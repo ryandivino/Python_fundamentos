b203071fc30e867f97782872fd0a1f9c758f08c0
ryan <ryankerllys1@gmail.com>
2026-03-29 14:30:55 -0300

    Refactor tabuada program for operator validation
    Refactor tabuada program to validate operator input and improve structure.

--- Tabuada.py
+++ Tabuada.py
@@ -1,3 +1,6 @@
+# Este programa solicita um número e um operador (+, -, *, /)
+# e exibe a tabuada de 1 até N aplicando a operação escolhida
+
 print("---------------")
 print("TABUADA SIMPLES")
 print("---------------")
@@ -8,33 +11,33 @@ print("---------------")
 op = input("Digite um operador: ")
 print("---------------")
 
+if op == "+" or op == "-" or op == "*" or op == "/":
 
-for i in range (1,n+1):
+    for i in range (1,n+1):
 
-    for k in range(1,11):
+        for k in range(1,11):
 
-        if op == "+":
-            resultado = i+k
-            print(f"{i} + {k} = {resultado}")
+            if op == "+":
+                resultado = i+k
+                print(f"{i} + {k} = {resultado}")
+                
+            elif op == "-":
             
-        elif op == "-":
-        
-            resultado = i-k
-            print(f"{i} - {k} = {resultado}")
+                resultado = i-k
+                print(f"{i} - {k} = {resultado}")
 
-        elif op == "*":
+            elif op == "*":
 
-            resultado = i*k
-            print(f"{i} X {k} = {resultado}")
-        
-        elif op == "/":
+                resultado = i*k
+                print(f"{i} X {k} = {resultado}")
+            
+            elif op == "/":
 
-            resultado = i/k
-            print(f"{i} / {k} = {resultado}")
-        
-        else:
+                resultado = i/k
+                print(f"{i} / {k} = {resultado}")
 
-            print("Por favor, insira um operador válido!")
+                print("---------------")
+            
+else:
 
-    print("---------------")
-
+    print("Por favor, insira um operador válido!")
