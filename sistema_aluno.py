fe98569641a62bfcb3961ab50db151c1dbc7d7fe
ryan <ryankerllys1@gmail.com>
2026-04-01 11:12:47 -0300

    Add files via upload

---
+++ sistema_aluno.py
@@ -0,0 +1,22 @@
+aluno = input("Digite o nome do aluno: ")
+soma = 0
+
+for i in range (1,4):
+
+    nota = float(input(f"Digite a nota {i} do aluno {aluno}: "))
+
+    soma = soma + nota
+
+media = soma/3
+
+if media >= 7:
+
+    print(f"O aluno {aluno} obteve média {media} e está APROVADO!")
+
+elif media >= 5:
+
+    print(f"O aluno {aluno} obteve média {media} e está de RECUPERAÇÃO!")
+
+else:
+
+    print(f"O aluno {aluno} obteve média {media} e está REPROVADO!")
