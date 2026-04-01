396a9dba9a96dbca4c42a16be166223028e37834
ryan <ryankerllys1@gmail.com>
2026-04-01 11:15:32 -0300

    Update Situacao_aluno.py

--- Situacao_aluno.py
+++ Situacao_aluno.py
@@ -1,4 +1,4 @@
-#Este algoritmo lê 3 notas de um aluno, calcula sua média e retorna a situação do aluno.
+#Este programa lê 3 notas de um aluno, calcula sua média e retorna a situação do aluno.
 
 aluno = input("Digite o nome do aluno: ")
 soma = 0
